784df89c8f0ea247b87f9059a5376b9fe5916bdd
史丽娜 <shilina_1123@outlook.com>
2016-01-14 10:21:51 +0800

    version1.0

---
+++ TestForGitHub/TestForGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestForGitHub
+//
+//  Created by 史丽娜 on 16/1/14.
+//  Copyright © 2016年 史丽娜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestForGitHub/TestForGitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestForGitHub
+//
+//  Created by 史丽娜 on 16/1/14.
+//  Copyright © 2016年 史丽娜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
