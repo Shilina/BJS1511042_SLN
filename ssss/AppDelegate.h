af80839278e8b618db0d4de3e1140770a337be70
史丽娜 <shilina_1123@outlook.com>
2016-01-14 10:24:53 +0800

    1.0

---
+++ ssss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ssss
+//
+//  Created by 史丽娜 on 16/1/14.
+//  Copyright © 2016年 史丽娜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
